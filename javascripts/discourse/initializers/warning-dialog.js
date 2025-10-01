f0c49abdbff2f1a336643362af2dbf90dace6a76
Cameron Wong <cam@camdar.io>
2025-10-01 12:53:16 -0400

    test
    Signed-off-by: Cameron Wong <cam@camdar.io>

--- javascripts/discourse/initializers/warning-dialog.js
+++ javascripts/discourse/initializers/warning-dialog.js
@@ -6,31 +6,27 @@ export default {
 
   initialize() {
     withPluginApi("0.11.7", (api) => {
-      api.modifyClass("controller:topic", {
-        pluginId: "discourse-admin-warnings",
-        dialog: service(),
-        actions: {
-          replyToPost(post, skipWarning = false) {
-            let lastPostedAt = moment(this.get('model.last_posted_at'));
-            let now = moment();
-            let diff = now - lastPostedAt;
-            let d = moment.duration(settings.death_timer);
+      api.modifyClass("service:composer", (Super) => class extends Super {
+        save(...args) {
+          let lastPostedAt = moment(this.model.last_posted_at);
+          let now = moment();
+          let diff = now - lastPostedAt;
+          let d = moment.duration(settings.death_timer);
 
-            if((diff >= d) && !skipWarning) {
-              // TODO: i18n this
-              let diffH = moment.duration(diff).humanize();
-              let confirmationMessage = `The last post in this thread was ${diffH} ago. Are you sure you want to bump this thread?`;
-              this.dialog.yesNoConfirm({
-                message: confirmationMessage,
-                didConfirm: () => {
-                  this.send("replyToPost", post, true);
-                }
-              });
-            } else {
-              return this._super(post);
-            }
-          },
-        },
+          if((diff >= d) && !skipWarning) {
+            // TODO: i18n this
+            let diffH = moment.duration(diff).humanize();
+            let confirmationMessage = `The last post in this thread was ${diffH} ago. Are you sure you want to bump this thread?`;
+            this.dialog.yesNoConfirm({
+              message: confirmationMessage,
+              didConfirm: () => {
+                this.send("replyToPost", post, true);
+              }
+            });
+          } else {
+            super.save(...args);
+          }
+        }
       });
     });
   },
